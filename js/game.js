d0ad836ae1b101a06371b4f0cca4f9888b8a9045
Catta <catalincatta@yahoo.com>
2023-10-30 19:57:18 +0200

    small issues and imporvements

--- js/game.js
+++ js/game.js
@@ -5,12 +5,10 @@ function checkBombs(x, y) {
         for (let j = -1; j <= 1; j++) {
             try {
 
-                document.querySelector(
-                    '[data-row*="' + (x + i).toString() + '"]' + '[data-col*="' + (y + j).toString() + '"]'
-                ).classList.contains('mine')
-                && counter++;
+                document.querySelector('[data-row*="' + (x + i).toString() + '"]' + '[data-col*="' + (y + j).toString() + '"]').classList.contains('mine') && counter++;
 
-            }catch (error){}
+            } catch (error) {
+            }
         }
     }
     return counter;
@@ -18,38 +16,26 @@ function checkBombs(x, y) {
 
 
 function emptyArea(x, y, doNotUseArr) {
-    doNotUseArr.push([x,y]);
+    doNotUseArr.push([x, y]);
 
     for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
 
-            if (doNotUseArr[doNotUseArr.length - 1][0] !== x + i
-                || doNotUseArr[doNotUseArr.length - 1][1] !== y + j){
+            if (doNotUseArr[doNotUseArr.length - 1][0] !== x + i || doNotUseArr[doNotUseArr.length - 1][1] !== y + j) {
 
                 try {
-                    if (!document.querySelector(
-                        '[data-row*="' + (x + i).toString() + '"][data-col*="' + (y + j).toString() + '"]'
-                        ).classList.contains('mine')
-
-                        && !document.querySelector(
-                            '[data-row*="' + (x + i).toString() + '"][data-col*="' + (y + j).toString() + '"]'
-                        ).classList.contains('open')
-                    ) {
-
+                    let element = document.querySelector(`[data-row*="${x + i}"][data-col*="${y + j}"]`);
+                    console.log(element)
+                    if (!element.classList.contains('mine') && !element.classList.contains('open') && !element.classList.contains("flagged")) {
                         let bombs = checkBombs((x + i), (y + j));
 
-                        document.querySelector(
-                            '[data-row*="' + (x + i).toString() + '"][data-col*="' + (y + j).toString() + '"]'
-                        ).classList.add('open');
+                        element.classList.add('open');
 
-                        bombs !== 0 ?
-                            document.querySelector(
-                                '[data-row*="' + (x + i).toString() + '"][data-col*="' + (y + j).toString() + '"]'
-                            ).innerHTML += bombs
-                            : emptyArea((x + i), (y + j), doNotUseArr);
+                        bombs !== 0 ? element.innerHTML = `&nbsp;${bombs.toString()}&nbsp;` : emptyArea((x + i), (y + j), doNotUseArr);
 
                     }
-                } catch (error) {}
+                } catch (error) {
+                }
             }
         }
     }
@@ -61,11 +47,9 @@ function flagsLeft() {
 
     for (let flag of document.querySelectorAll('.field')) {
 
-        flag.classList.contains('mine')
-        && counter++;
+        flag.classList.contains('mine') && counter++;
 
-        flag.classList.contains('flagged')
-        && counter--;
+        flag.classList.contains('flagged') && counter--;
 
     }
     return counter;
@@ -78,53 +62,75 @@ function checkWin() {
 
     for (let opened of document.querySelectorAll('.field')) {
 
-        opened.classList.contains('open')
-        && hitCounter++;
+        opened.classList.contains('open') && hitCounter++;
 
-        opened.classList.contains('mine')
-        && bombCounter++;
+        opened.classList.contains('mine') && bombCounter++;
 
     }
-    return document.querySelectorAll('.field').length
-        === hitCounter + bombCounter;
+    return document.querySelectorAll('.field').length === hitCounter + bombCounter;
 }
 
 
-function countUp(){
+function countUp() {
     let counter = 0;
 
     let interval = setInterval(function () {
         counter++;
 
-        document.getElementById("elapsed-time-counter").value
-            = Math.floor(counter / 250).toString();
+        document.getElementById("elapsed-time-counter").value = Math.floor(counter / 250).toString();
 
         if (document.querySelector('.game-field')
-                .getAttribute('class')
-            === 'game-field end'
-        ) {
+            .getAttribute('class') === 'game-field end') {
             clearInterval(interval);
         }
     });
 }
 
+function switchFlagType() {
+    const myButton = document.getElementById('changeFlagType');
+    if (localStorage.getItem('flag') == null) {
+        myButton.classList.remove('flag-disabled');
+        myButton.classList.add('flag-enabled');
+        localStorage.setItem('flag', " ");
+    } else {
+        myButton.classList.remove('flag-enabled');
+        myButton.classList.add('flag-disabled');
+        localStorage.removeItem("flag");
+    }
+}
+
 
 const game = {
     init: function () {
+        localStorage.removeItem("flag");
         this.drawBoard();
         document.querySelector('#flags-left-counter')
             .setAttribute('value', flagsLeft().toString());
-        // TODO: do the rest of the game setup here (eg. add event listeners)
         this.initRightClick();
         this.initLeftClick();
+        document.getElementById('changeFlagType').addEventListener('click', switchFlagType);
     },
 
     drawBoard: function () {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
-        const rows = parseInt(urlParams.get('rows'));
-        const cols = parseInt(urlParams.get('cols'));
-        const mineCount = parseInt(urlParams.get('mines'));
+
+        if (localStorage.getItem('rows') == null) localStorage.setItem('rows', '10');
+        if (localStorage.getItem('cols') == null) localStorage.setItem('cols', '10');
+        if (localStorage.getItem('mines') == null) localStorage.setItem('mines', '10');
+
+        let tempRow = urlParams.get('rows');
+        let tempCols = urlParams.get('cols');
+        let tempMineCount = urlParams.get('mines');
+
+        let rows = tempRow != null ? parseInt(tempRow) : localStorage.getItem('rows');
+        let cols = tempCols != null ? parseInt(tempCols) : localStorage.getItem('cols');
+        let mineCount = tempMineCount != null ? parseInt(tempMineCount) : localStorage.getItem('mines');
+
+        localStorage.setItem('rows', rows)
+        localStorage.setItem('cols', cols)
+        localStorage.setItem('mines', mineCount)
+
         const minePlaces = this.getRandomMineIndexes(mineCount, cols, rows);
 
         let gameField = document.querySelector(".game-field");
@@ -137,112 +143,90 @@ const game = {
                 cellIndex++;
             }
         }
-    },
-    getRandomMineIndexes: function (mineCount, cols, rows) {
+    }, getRandomMineIndexes: function (mineCount, cols, rows) {
         const cellCount = cols * rows;
         let mines = new Set();
         do {
             mines.add(Math.round(Math.random() * (cellCount - 1)));
         } while (mines.size < mineCount && mines.size < cellCount);
         return mines;
-    },
-    setGameFieldSize: function (gameField, rows, cols) {
-        gameField.style.width = (gameField.dataset.cellWidth * rows) + 'px';
-        gameField.style.height = (gameField.dataset.cellHeight * cols) + 'px';
-    },
-    addRow: function (gameField) {
-        gameField.insertAdjacentHTML(
-            'beforeend',
-            '<div class="row"></div>'
-        );
+    }, setGameFieldSize: function (gameField, rows, cols) {
+        gameField.style.width = (gameField.dataset.cellWidth * cols) + 'px';
+        gameField.style.height = (gameField.dataset.cellHeight * rows) + 'px';
+    }, addRow: function (gameField) {
+        gameField.insertAdjacentHTML('beforeend', '<div class="row"></div>');
         return gameField.lastElementChild;
-    },
-    addCell: function (rowElement, row, col, isMine) {
-        rowElement.insertAdjacentHTML(
-            'beforeend',
-            `<div class="field${isMine ? ' mine' : ''}"
+    }, addCell: function (rowElement, row, col, isMine) {
+        rowElement.insertAdjacentHTML('beforeend', `<div class="field${isMine ? ' mine' : ''}"
                         data-row="${row}"
-                        data-col="${col}">&nbsp;</div>`);
-    },
-    // reference solution for "Create mine flagging feature" user story
-    initRightClick() {
-        // we collect all fields of the game.
-        // (the same selector is used as in the style.css file for finding the fields)
+                        data-col="${col}">&nbsp;&nbsp;&nbsp;&nbsp;</div>`);
+    }, initRightClick() {
         const fields = document.querySelectorAll('.game-field .row .field');
 
-        // for all fields...
         for (let field of fields) {
-            // we add the same event listener for the right click (so called contextmenu) event
-            field.addEventListener('contextmenu', async function (event) {
-                // so if you right-click on any field...
+            field.addEventListener('contextmenu', async function (e) {
+                flagHandle(field, e)
+            });
+        }
+    }, initLeftClick() {
+        for (let field of document.querySelectorAll('.field')) {
 
-                // context menu remains hidden
-                event.preventDefault();
-
-                // and "flagged" class toggles on the clicked element
-                // (styles of "flagged" class are defined in style.css)
-                event.currentTarget.classList.toggle('flagged');
-
-                document.querySelector('#flags-left-counter')
-                    .setAttribute('value', flagsLeft().toString());
-
-                document.getElementById("elapsed-time-counter").value === ''
-                && countUp('start');
+            field.addEventListener('click', function (e) {
+                beehive(field, e);
             });
         }
     },
-    initLeftClick() {
-        for (
-            let field of document.querySelectorAll('.field'
-        )) {
-
-            field.addEventListener(
-                'click', function (event) {
-
-                    if (field.className === 'field') {
-                        event.currentTarget.classList.add('open');
-
-                        let bombs = checkBombs(
-                            parseInt(field.getAttribute('data-row')),
-                            parseInt(field.getAttribute('data-col'))
-                        );
-
-                        if (bombs === 0) {
-                            field.innerHTML = ""
-
-                            emptyArea(
-                                parseInt(field.getAttribute('data-row')),
-                                parseInt(field.getAttribute('data-col')),
-                                []
-                            );
-
-                        }else {
-                            field.innerHTML += bombs.toString()
-                        }
-
-                    } else if (field.className === 'field mine') {
-
-                        for (
-                            let mine of document.getElementsByClassName('field mine')
-                            ) {
-                            mine.classList.add('bomb');
-                        }
-
-                        alert('LOSE :((');
-                        document.querySelector('.game-field')
-                            .classList.add('end');
-                    }
-
-                    checkWin() &&(alert('!!!WIN!!!'),
-                        document.querySelector('.game-field')
-                            .classList.add('end'));
-
-                    document.getElementById("elapsed-time-counter").value === ''
-                    && countUp('start');
-            });
-        }
-    },
-
 };
+
+function beehive(field, e) {
+    if (localStorage.getItem('flag') == null) {
+        areaHandle(field, e);
+    } else {
+        flagHandle(field, e);
+    }
+}
+
+function flagHandle(field, event) {
+    if (field.className.includes('open')) return;
+    if (flagsLeft() <= 0) return;
+
+    event.preventDefault();
+    event.currentTarget.classList.toggle('flagged');
+
+    document.querySelector('#flags-left-counter')
+        .setAttribute('value', flagsLeft().toString());
+
+    document.getElementById("elapsed-time-counter").value === '' && countUp('start');
+}
+
+function areaHandle(field, event) {
+    if (field.className === 'field') {
+        event.currentTarget.classList.add('open');
+
+        let bombs = checkBombs(parseInt(field.getAttribute('data-row')), parseInt(field.getAttribute('data-col')));
+
+        if (bombs === 0) {
+            field.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;"
+
+            emptyArea(parseInt(field.getAttribute('data-row')), parseInt(field.getAttribute('data-col')), []);
+
+        } else field.innerHTML = `&nbsp;${bombs.toString()}&nbsp;`;
+
+    } else if (field.className === 'field mine') {
+
+        for (let mine of document.getElementsByClassName('field mine')) {
+            mine.classList.add('bomb');
+        }
+
+        alert('LOSE :((');
+        document.querySelector('.game-field')
+            .classList.add('end');
+    }
+
+    checkWin() && (alert('!!!WIN!!!'), document.querySelector('.game-field')
+        .classList.add('end'));
+
+    document.getElementById("elapsed-time-counter").value === '' && countUp('start');
+}
 
 game.init()
